bbf5619178421a09fad3fb873de15caf49e87b66
Joseph Hilby <joseph.hilby@gmail.com>
2025-12-28 11:15:16 -0600

    initial compiler complete

---
+++ examples/dist/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.exit(new Main().main());
+    }
+
+    int main() {
+        System.out.println("Hello World");
+        return 0;
+    }
+
+}
